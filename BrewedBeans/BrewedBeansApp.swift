c829e4ef3fa2a6aa9e1265794e3cb8bd6d1a2f6b
nathan102030 <79672895+nathan102030@users.noreply.github.com>
2023-08-31 21:14:10 -0400

    Add files via upload

---
+++ BrewedBeans/BrewedBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewedBeansApp.swift
+//  BrewedBeans
+//
+//  Created by Nathan Demssie on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BrewedBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrewedBeans/Menu.swift
@@ -0,0 +1,30 @@
+//
+//  Menu.swift
+//  BrewedBeans
+//
+//  Created by Nathan Demssie on 8/19/23.
+//
+
+import Foundation
+
+class Menu: ObservableObject, Codable{
+    let sections: [MenuSection]
+    var milkOptions = [ConfigurationOption.none]
+    var syrupOptions = [ConfigurationOption.none]
+    
+    init(){
+        do{
+            let url = Bundle.main.url(forResource: "menu", withExtension: "json")!
+            let data = try Data(contentsOf: url)
+            let menuData = try JSONDecoder().decode(Menu.self, from: data)
+            
+            sections = menuData.sections
+            milkOptions.append(contentsOf: menuData.milkOptions)
+            syrupOptions.append(contentsOf: menuData.syrupOptions)
+            
+        } catch{
+            fatalError("menu.json is missing or corrupt")
+        }
+    }
+}
+
